be277f79cba1815111c97541edda214a0b631bf5
kamil <kamil.kurzaj04@gmail.com>
2025-05-30 10:24:50 +0200

    Added right-sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
+import {RightSidebar} from './right-sidebar/right-sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, RightSidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
