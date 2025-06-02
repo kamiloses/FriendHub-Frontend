0775c233b804c0d73c0a71ec58aa8e58a12c6295
kamil <kamil.kurzaj04@gmail.com>
2025-06-02 13:12:09 +0200

    Created HomeLayout

---
+++ src/app/home-layout/home-layout.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {Header} from "../header/header";
+import {LeftSidebar} from "../left-sidebar/left-sidebar";
+import {RightSidebar} from "../right-sidebar/right-sidebar";
+
+@Component({
+  selector: 'app-home-layout',
+  standalone:true,
+    imports: [
+        Header,
+        LeftSidebar,
+        RightSidebar
+    ],
+  templateUrl: './home-layout.html',
+  styleUrl: './home-layout.css'
+})
+export class HomeLayout {
+
+}
