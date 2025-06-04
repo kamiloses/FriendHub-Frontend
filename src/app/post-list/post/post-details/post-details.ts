7f4a48e982745765bb9bc2149c3ac3f7527d4779
kamil <kamil.kurzaj04@gmail.com>
2025-06-04 11:56:38 +0200

    Added validation in postDetails

--- src/app/post-list/post/post-details/post-details.ts
+++ src/app/post-list/post/post-details/post-details.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PostDetailsService} from './post-details.service';
 import {PostModel} from '../../../models/post.model';
+import {map} from 'rxjs';
 
 @Component({
   imports: [],
@@ -9,28 +10,30 @@ import {PostModel} from '../../../models/post.model';
   templateUrl: './post-details.html',
   styleUrls: ['./post-details.css'],
 })
-export class PostDetailsComponent implements OnInit{
-  constructor(private postDetailsService:PostDetailsService) {
+export class PostDetailsComponent implements OnInit {
+  constructor(private postDetailsService: PostDetailsService) {
   }
 
   public postModel: Partial<PostModel> = {}; //todo nauczyc sie partial
-
-
+  postFetchError: string | null = null;
 
 
   ngOnInit(): void {
-        this.postDetailsService.getSpecificPost("683f0ad2ce5a4c4a70c07f53").subscribe({
-          next:(post:PostModel)=>{
-            this.postModel=post;},
-          error:(error)=>{
-            console.log(error)
-
-    }
-
-
-        })
-
-    }
+    this.postDetailsService.getSpecificPost("683f0ad2ce5a4c4a70c07f53").pipe(
+      map(post => {
+        if (!post) {
+          throw new Error("Post is null");
+        }
+        return post;
+      }))
+      .subscribe({
+        next: (post: PostModel) => {
+          this.postModel = post;
+        },
+        error: (error) => {
+          console.log(error)
+          this.postFetchError = "There was some Problem with Fetching post data"}}
+      )}
 
 
 }
