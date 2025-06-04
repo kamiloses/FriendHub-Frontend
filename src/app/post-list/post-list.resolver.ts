fdd219b3e08c4ca088bf9476ad2468cf70a25e72
kamil <kamil.kurzaj04@gmail.com>
2025-06-04 09:49:21 +0200

    Moved logic to post list service

--- src/app/post-list/post-list.resolver.ts
+++ src/app/post-list/post-list.resolver.ts
@@ -1,22 +1,19 @@
-import { ResolveFn } from '@angular/router';
+import {ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot} from '@angular/router';
 import { PostModel } from '../models/post.model';
 import { inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {catchError, Observable, of} from 'rxjs';
+import {PostListService} from './post-list.service';
 
 export const postListResolver: ResolveFn<PostModel[]|null> = (
-  route,
-  state
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
 ): Observable<PostModel[]|null> => {
-  const httpClient = inject(HttpClient);
-  const username = 'kamiloses1';
+  const postListService= inject(PostListService);
 
-  return httpClient.get<PostModel[]>(`http://localhost:8080/api/posts?username=${username}`)
-    .pipe(
-      catchError(err => {
-        console.error('Resolver error:', err);
-        return of(null);
-      }))
+  return postListService.getAllPosts()
 
-    ;
+
+
+
 };
--- src/app/post-list/post-list.service.ts
+++ src/app/post-list/post-list.service.ts
@@ -1,7 +1,27 @@
+import {Injectable} from '@angular/core';
+import {PostModel} from '../models/post.model';
+import {catchError, Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+
+@Injectable({ providedIn: 'root' })
 export class PostListService {
 
 
+  constructor(private http: HttpClient) {
+  }
 
-  
+
+  getAllPosts(): Observable<PostModel[] | null> {
+    const username = "kamiloses1"
+    return this.http
+      .get<PostModel[]>(`http://localhost:8080/api/posts?username=${username}`)
+      .pipe(
+        catchError(err => {
+          console.error('Post Module  error:', err);
+          return of(null);
+        })
+      );
+  }
 
 }
