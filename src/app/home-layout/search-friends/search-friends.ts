7031867792e637767aa6c112e24438c20eed1f0b
kamil <kamil.kurzaj04@gmail.com>
2025-06-28 06:41:24 +0200

    Created search-friends component

---
+++ src/app/home-layout/search-friends/search-friends.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-friends',
+  imports: [],
+  templateUrl: './search-friends.html',
+  styleUrl: './search-friends.css'
+})
+export class SearchFriends {
+
+}
