62150e43882bfeaec9dd9c987a6588871c0eb4a3
kamil <kamil.kurzaj04@gmail.com>
2025-06-30 07:04:46 +0200

    Modified searchUser service

--- src/app/home-layout/search-user/search-user.service.ts
+++ src/app/home-layout/search-user/search-user.service.ts
@@ -2,11 +2,12 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SearchedUserModel} from './searched-user.model';
 import {Injectable} from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'})
 export class SearchUserService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,private authService:AuthService) {
   }
   //todo zmien nazwe z friendsMOdel
   searchPeople(username:string): Observable<SearchedUserModel[]> {
@@ -16,4 +17,26 @@ export class SearchUserService {
     });
     return this.http.get<SearchedUserModel[]>("http://localhost:8084/api/friends/"+username,{headers});
   }
+
+  addFriend(friendUsername:string){
+    const headers = new HttpHeaders({
+      'friendUsername': friendUsername,
+      'myUsername': this.authService.getUsername()||''//todo popraw to '' potem
+    });
+
+    return this.http.post("http://localhost:8084/api/friends",{headers})
+
+  }
+
+  removeFriend(friendUsername:string){
+    const headers = new HttpHeaders({
+      'friendUsername': friendUsername,
+      'myUsername': this.authService.getUsername()||''//todo popraw to '' potem
+    });
+
+    return this.http.delete("http://localhost:8084/api/friends",{headers})
+
+  }
+
+
 }
--- src/app/home-layout/search-user/search-user.ts
+++ src/app/home-layout/search-user/search-user.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {SearchedUserModel} from './searched-user.model';
+import {SearchUserService} from './search-user.service';
 
 @Component({
   selector: 'app-search-user',
@@ -11,7 +12,7 @@ import {SearchedUserModel} from './searched-user.model';
 })
 export class SearchUser implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,private searchUserService:SearchUserService) {
   }
 
 
@@ -20,18 +21,25 @@ export class SearchUser implements OnInit {
 
   ngOnInit() {
     this.fetchUsers();
+  }
 
 
+  onClickUserMyFriend(){
 
+    this.searchUserService.removeFriend("dsadadsaddsadsa").subscribe()
 
   }
+
+  onClickUserNotFriend(){//todo zmien nazwe
+    this.searchUserService.addFriend("dsadadsaddsadsa").subscribe()
+
+  }
+
 
 
   fetchUsers() {
     this.route.data.subscribe(data => {
       this.searchedUsers = data['searchedUsers'];
     });//todo obsluzyc jeszcze jakos
-
-
   }
 }
