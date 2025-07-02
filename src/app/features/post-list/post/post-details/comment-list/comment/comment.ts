e65babced2662e459b4bf5603140c4172ae9549b
kamil <kamil.kurzaj04@gmail.com>
2025-07-02 20:35:19 +0200

    changed comment.html Added possibility to send replies

--- src/app/features/post-list/post/post-details/comment-list/comment/comment.ts
+++ src/app/features/post-list/post/post-details/comment-list/comment/comment.ts
@@ -1,19 +1,45 @@
 import {Component, Input} from '@angular/core';
 import {CommentModel} from './comment.model';
+import {FormsModule} from '@angular/forms';
+import {CommentListService} from '../comment-list.service';
+import {PublishCommentModel} from '../publish-comment.model';
 
 @Component({
   selector: 'app-comment',
-    imports: [
-    ],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './comment.html',
   styleUrl: './comment.css',
   standalone:true
 })
 export class Comment {
   @Input() comment!: CommentModel;
+  @Input({required: true}) currentRoute!: string;
   isExpanded = false;
+
+  constructor(private commentListService:CommentListService) {}
 
   toggleExpand() {
     this.isExpanded = !this.isExpanded;
   }
+
+  sendReply(content:string,parentCommentId:string){
+
+    const commentModel: PublishCommentModel = {
+      postId: this.currentRoute,
+      parentCommentId: parentCommentId,
+      content: content
+    };
+
+    console.log("HEJ"+commentModel.parentCommentId)
+
+    this.commentListService.sendComment("kamiloses1",commentModel).subscribe()
+
+
+
+
+  }
+
+
 }
