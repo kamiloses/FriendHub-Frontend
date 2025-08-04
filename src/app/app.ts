64aa4b1584ea85a91a2a98e6ca6b6eead2f91f8f
kamil-kurzaj <kamil.kurzaj04@gmail.com>
2025-08-04 11:25:06 +0200

    Added register.html forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import {LoginComponent} from './auth/login/login';
+import {Register} from './auth/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [LoginComponent, Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
