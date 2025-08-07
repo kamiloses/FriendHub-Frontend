eb70f906ea5f27ebd30a249271cc8e14335a5728
kamil-kurzaj <kamil.kurzaj04@gmail.com>
2025-08-07 12:15:35 +0200

    Modified Post-Details component

--- src/app/home/post-list/post/post-details/post-details.service.ts
+++ src/app/home/post-list/post/post-details/post-details.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Post} from '../post';
+import {PostModelResponse} from '../post-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostDetailsService {
-  
+
+  constructor(private http: HttpClient) {}
+
+  private readonly apiUrl = 'http://localhost:8080/api/posts';
+
+
+     getPostById(id: number): Observable<PostModelResponse|null> {
+
+    return this.http.get<PostModelResponse|null>(`${this.apiUrl}/6894712ae3f65640d161ead1`);
+  }
+
 }
--- src/app/home/post-list/post/post-details/post-details.ts
+++ src/app/home/post-list/post/post-details/post-details.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {PostModelResponse} from '../post-response.model';
+import {PostDetailsService} from './post-details.service';
 
 @Component({
   selector: 'app-post-details',
@@ -10,9 +11,44 @@ import {PostModelResponse} from '../post-response.model';
   templateUrl: './post-details.html',
   styleUrl: './post-details.css'
 })
-export class PostDetails {
+export class PostDetails implements OnInit {
   text: any;
-  post!: PostModelResponse;
+
+
+  post = signal<PostModelResponse | null>(null)
+  serverError = signal<string | null>(null)
+  isLoading = signal<boolean>(false)
+
+  constructor(private postDetailsService: PostDetailsService) {
+  }
+
+  ngOnInit(): void {
+  this.loadPosts()
+
+  }
+
+  loadPosts(): void {
+    this.isLoading.set(true);
+    this.postDetailsService.getPostById(100).subscribe({
+        next: fetchedPost => {
+          this.post.set(fetchedPost)
+          this.isLoading.set(false);
+
+        }
+        , error: err => {
+          console.error(err);
+          this.serverError.set("There was an error fetching post.");
+          this.isLoading.set(false);
+        }
+      }
+    )
+
+  }
+
+
+  reloadPosts(): void {
+    this.loadPosts();
+  }
 
   onPublish() {
 
