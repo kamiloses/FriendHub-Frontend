eaf0d315fc946f8cefc17603aed842ded0031f58
kamiloses <kamil.kurzaj04@gmail.com>
2026-04-18 18:58:01 +0200

    Code refactoring

--- src/app/home/home-layout.ts
+++ src/app/home/home-layout.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header/header.component';
+import {RightSidebarComponent} from './right-sidebar/right-sidebar.component';
 
 @Component({
   selector: 'app-home-layout',
@@ -8,6 +9,7 @@ import {HeaderComponent} from './header/header.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
+    RightSidebarComponent,
   ],
   templateUrl: './home-layout.html',
   styleUrl: './home-layout.css'
